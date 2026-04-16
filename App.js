06680eeb2a3e8a8ae01ea1cc75c0376adc406c2f
PietroDonella <pietrosalomao1@gmail.com>
2026-04-16 17:36:43 -0300

    Arrumando App.js e instalando dependências.

--- App.js
+++ App.js
@@ -1,40 +1,26 @@
-import { useEffect, useState } from "react";
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Cadastro from "./src/screens/Cadastro";
+import Perfil from "./src/screens/Perfil";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [nome, setNome] = useState("");
-  const [curso, setCurso] = useState("");
-  const [disciplina, setDisciplina] = useState("");
-  const [descricao, setDescricao] = useState("");
-
-  const [enviar, setEnviar] = useState(false);
-
-  useEffect(() => {
-    console.log("CP2 - Pronto para uso.");
-  }, []);
-
-  const gerenciarEnvio = () => {
-    setEnviar(true);
-  };
-
-  const reiniciarFormulario = () => {
-    setNome("");
-    setCurso("");
-    setDisciplina("");
-    setDescricao("");
-    setEnviar(false);
-  };
-
   return (
-    <Cadastro/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Cadastro">
+        <Stack.Screen 
+          name="Cadastro" 
+          component={Cadastro} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Perfil" 
+          component={Perfil} 
+          options={{ title: 'Meu Perfil' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
